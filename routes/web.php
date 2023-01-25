629ee96ae2e2217d38aa440034de7d21354556bf
sanbray99 <ikhsannawawi99@gmail.com>
2023-01-25 13:17:57 +0700

    Commit Pertama

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\landingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('landing.home');
+// });
+
+Route::get('/', [landingController::class, 'index'])->name('index');
+Route::get('/pertandingan', [landingController::class, 'pertandingan'])->name('pertandingan');
+Route::get('/support', [landingController::class, 'support'])->name('support');
+Route::get('/blog', [landingController::class, 'blog'])->name('blog');
+Route::get('/tentang-kami', [landingController::class, 'tentang-kami'])->name('tentang-kami');
